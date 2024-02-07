4325942d3f88f9cd0082c8009f7e977aeaec70b5
João Chaves <joao.chaves@liferay.com>
2024-02-06 22:40:35 -0300

    first commit

---
+++ src/main/java/br/com/zrteam/managementapp/ManagementappApplication.java
@@ -0,0 +1,13 @@
+package br.com.zrteam.managementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementappApplication.class, args);
+	}
+
+}
