6c42f74ddf077c68efaa6fc5bd784b58ebf9b6fb
João Chaves <joao.chaves@liferay.com>
2024-02-17 20:29:07 -0300

    feat: add user jpa repository

---
+++ src/main/java/br/com/zrteam/managementapp/infra/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.zrteam.managementapp.infra.repository;
+
+import br.com.zrteam.managementapp.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
+}
